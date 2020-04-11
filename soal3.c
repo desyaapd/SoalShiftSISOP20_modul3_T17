4ac8695cc438d87d8a17ed4780840d1cc8e960c4
rindikar <49342639+rindikar@users.noreply.github.com>
2020-04-11 18:21:49 +0700

    Add files via upload

--- soal3.c
+++ soal3.c
@@ -1,227 +1,175 @@
-#include<stdlib.h>
-#include<string.h>
-#include<sys/stat.h>
-#include<pthread.h>
-#include<sys/wait.h>
-#include<stdio.h>
-#include<sys/types.h>
-#include<unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <pthread.h>
+#include <errno.h>
 
-#define MAX 256
- 
+char *getFileName(char *fName, char buff[]) {
+  char *token = strtok(fName, "/");
+  while (token != NULL) {
+    sprintf(buff, "%s", token);
+    token = strtok(NULL, "/");
+  }
+}
 
-static void NamaFileStrip(char *NamaFile, char *NamaFileBaru, char *ext);
-void* buatDir(void *data);
-void* move(void *data);
-void* thread(void *arg);
+char *getExtension(char *fName, char buff[]) {
+  char buffFileName[1337];
+  char *token = strtok(fName, "/");
+  while (token != NULL) {
+    sprintf(buffFileName, "%s", token);
+    token = strtok(NULL, "/");
+  }
+  int count = 0;
+  token = strtok(buffFileName, ".");
+  while(token != NULL) {
+    count++;
+    sprintf(buff, "%s", token);
+    token = strtok(NULL, ".");
+  }
+  if (count <= 1) {
+    strcpy(buff, "unknown");
+  }
 
-//struct
+  return buff;
+}
 
-struct var
-{
-    char *nf[1];
-    char *e[1];
-    char *p[1];
-    char *pa[1];
+void dirChecking(char buff[]) {
+  DIR *dr = opendir(buff);
+  if (ENOENT == errno) {
+    mkdir(buff, 0775);
+    closedir(dr);
+  }
+}
+
+struct args {
+  char *buffer;
 };
 
+void *routine(void* arg) {
+  char buffExt[100];
+  char buffFileName[1337];
+  char buffFrom[1337];
+  char buffTo[1337];
+  char cwd[1337];
+  getcwd(cwd, sizeof(cwd));
+  strcpy(buffFrom, (char *) arg);
 
-//Global Variable
+  if (access(buffFrom, F_OK) == -1) {
+    printf("File %s tidak ada\n", buffFrom);
+    pthread_exit(0);
+  }
+  DIR* dir = opendir(buffFrom);
+  if (dir) {
+    printf("file %s berupa folder\n", buffFrom);
+    pthread_exit(0);
+  }
+  closedir(dir);
 
+  getFileName(buffFrom, buffFileName);
+  strcpy(buffFrom, (char *) arg);
 
-int status;
-pthread_t tid[3];
+  getExtension(buffFrom, buffExt);
+  for (int i = 0; i < sizeof(buffExt); i++) {
+    buffExt[i] = tolower(buffExt[i]);
+  }
+  strcpy(buffFrom, (char *) arg);
 
+  dirChecking(buffExt);
 
-int main(int argc, char* argv[])
-{
-    // Declare variable
-    char ext[5];
-    char NamaFile[31];
-    char NamaFileBaru[31];
-    char pathawal[50];
-    char pathakhir[50];
-    char star[] = "*";
-    char f[] = "-f";
-    char d[] = "-d";
+  sprintf(buffTo, "%s/%s/%s", cwd, buffExt, buffFileName);
+  rename(buffFrom, buffTo);
 
-    int i = 2;
-
-    //Arg = -f
-    if (strcmp (argv[1],f) == 0)
-    {
-        //Looping arg
-        for (; i < argc; i++)
-        {
-            //Memory Set
-            memset(ext, 0, sizeof(ext));
-          
-            memset(NamaFile, 0, sizeof(NamaFile));
-            memset(NamaFileBaru, 0, sizeof(NamaFileBaru));
-    
-            memset(pathawal, 0, sizeof(pathawal));
-            memset(pathakhir, 0, sizeof(pathakhir));
-
-            //Cari File Path dant Ext
-            strcpy(NamaFile, argv[i]);
-            NamaFileStrip(NamaFile, NamaFileBaru, ext);
-            strcat(NamaFileBaru, ",");
-            strcat(NamaFileBaru, ext);
-            strcat(pathawal, "/home/xd/Desktop/Modul3/shift/");
-            strcat(pathawal, NamaFileBaru);
-            strcat(pathakhir, "/home/xd/Desktop/Modul3/");
-            strcat(pathakhir, ext);
-            strcat(pathakhir, "/");
-            strcat(pathakhir, NamaFileBaru);
-
-            // ditaruh dalam struct
-            struct var *data = (struct var*) malloc (sizeof (struct var));
-            
-            *data -> e = ext;
-            *data -> nf = NamaFileBaru;
-            *data -> pa = pathakhir;
-            *data -> p = pathawal;
-
-            int t = 0;
-            status = 0;
-
-            //membuat thread
-            while (t<2)
-            {
-                if(t==0){
-                    pthread_create(&(tid[t]),NULL,&buatDir,(void *) data);
-                } else {
-                    pthread_create(&(tid[t]),NULL,&move,(void *) data);
-                }
-                pthread_join(tid[t],NULL);
-                t++;
-            }
-        }
-    }
-
-     if (strcmp (argv[1],d) == 0)
-    {
-
-    }
-
-    else (strcmp (argv[1],star) == 0)
-    {
-
-    }
- 
- 
-    return 0;
-}
- 
-static void NamaFileStrip(char *NamaFile, char *NamaFileBaru, char *ext)
-{
-    int PLocation;
-    int i, j, nf, e, p;
- 
-    i = strlen(NamaFile) - 1;
- 
-    //Cari periode nama file awal
-    while (i && NamaFile[i] != '.')
-    {
-        i--;
-    }
- 
-    // if i > 0, Nama file ada periode
-    if (i)
-    {
-        PLocation = i;
-    }
-    else
-    {
-        //Ga ad periode jd copynamafile
-        PLocation = strlen(NamaFile);
- 
-        //Reset i untuk nyari separator
-        i = strlen(NamaFile) - 1;
-    }
- 
-    //Cari separator
-    while (i && NamaFile[i] != '/')
-    {
-        if (NamaFile[i] != '/')
-            i--;
-    }
- 
-    //Kalo ga ad separatornya  mundur 1 char
-    if (NamaFile[i] == '/')
-        i++;
-         
-    nf = 0;
-    
-    //Copy file yang dari setelah separator
-    while (i < PLocation)
-    {
-        NamaFileBaru[nf] = NamaFile[i];
-        nf++;
-        i++;
-    }
-    
-    //copy ekstensi
-
-    e = 0;
-    j = strlen(NamaFile) - 1;
-    while (i<j)
-    {
-        ext[e] = NamaFile[i+1];
-        e++;
-        i++;
-    }
-    
-    NamaFileBaru[nf] = '\0';
-    ext[e] = '\0';
+  pthread_exit(0);
 }
 
-void* buatDir(void *parameter)
-{
-    struct var *par = (struct var*) parameter;
+int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    printf("Argument kurang\n");
+    exit(1);
+  }
+  if (strcmp(argv[1], "-f") != 0 && strcmp(argv[1], "*") != 0 && strcmp(argv[1], "-d")) {
+    printf("Argument tidak ada\n");
+    exit(1);
+  }
 
-    status = 1;
-
-    mkdir(*par->e, 0777);
-
-	return NULL;
-}
-
-void* move(void *parameter)
-{
-    struct var *par = (struct var*) parameter;
-
-    while (status != 1)
-    {
-
-    }
-    
-    int ch;
-    FILE *fp1, *fp2;
-
-    fp1 = fopen(*par->p, "r");
-    fp2 = fopen(*par->pa, "w");
-
-    //ERRROR
-    if (!fp1) {
-            printf("Unable to open source file to read!!\n");
-            fclose(fp2);
-            return 0;
-    }
-    if (!fp2) {
-            printf("Unable to open target file to write\n");
-            return 0;
-    }
-    //Copy isi file ke target file
-    while ((ch = fgetc(fp1)) != EOF) {
-            fputc(ch, fp2);
+  if (strcmp(argv[1], "-f") == 0) {
+    if (argc <= 2) {
+      printf("Argument salah\n");
+      exit(1);
     }
 
-    //Tutup File
-    fclose(fp1);
-    fclose(fp2);
-    //Remove source file
-    remove(*par->p);
+    pthread_t tid[argc-2];
+    for (int i = 2; i < argc; i++) {
+      pthread_create(&tid[i-2], NULL, &routine, (void *)argv[i]);
+    }
+    for (int i = 2; i < argc; i++) {
+      pthread_join(tid[i-2], NULL);
+    }
+    exit(0);
+  }
 
-	return NULL;
+  char *directory;
+  if (strcmp(argv[1], "*") == 0) {
+    if (argc != 2) {
+      printf("Argument salah\n");
+      exit(1);
+    }
+    char buff[1337];
+    getcwd(buff, sizeof(buff));
+    directory = buff;
+  }
+
+  if (strcmp(argv[1], "-d") == 0) {
+    if (argc != 3) {
+      printf("Argument salah\n");
+      exit(1);
+    }
+    DIR* dir = opendir(argv[2]);
+    if (dir) {
+      directory = argv[2];
+    } else if (ENOENT == errno) {
+      printf("Directory tidak ada\n");
+      exit(1);
+    }
+    closedir(dir);
+  }
+
+  int file_count = 0;
+  DIR* dir = opendir(directory);
+  struct dirent *entry;
+  while ((entry = readdir(dir)) != NULL) {
+    if (entry->d_type == DT_REG) {
+      file_count++;
+    }
+  }
+  closedir(dir);
+
+  pthread_t tid[file_count];
+  char buff[file_count][1337];
+  int iter = 0;
+
+  dir = opendir(directory);
+  while ((entry = readdir(dir)) != NULL) {
+    if (entry->d_type == DT_REG) {
+      sprintf(buff[iter], "%s/%s", directory, entry->d_name);
+      iter++;
+    }
+  }
+  closedir(dir);
+
+  for (int i = 0; i < file_count; i++) {
+    char  *test = (char*)buff[i];
+    printf("%s\n", test);
+    pthread_create(&tid[i], NULL, &routine, (void *)test);
+  }
+
+  for (int i = 0; i < file_count; i++) {
+    pthread_join(tid[i], NULL);
+  }
+
 }
